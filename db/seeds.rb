66b8c20869596d4a67a1b37d54f4c57c2df4c6fe
Dorothy Brown <dorothy.k.brown@gmail.com>
2019-05-27 16:58:43 +0100

    added view for user to input location

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning DB..."
+User.destroy_all
+Itinerary.destroy_all
+
+puts "Creating Seeds..."
+# User
+ user_info = {
+  email: "dorothy@whimaway.com",
+  encrypted_password: "123456",
+  name: "Dorothy"
+}
+
+user = User.create(user_info)
+
+# Itinerary
+itinerary_info = {
+  location: "Lisbon, Portugal",
+  search_radius: "5 km",
+  available_time: 3, # hours
+  name: "Lisbon Itinerary",
+  user_id: user.id
+}
+
+Itinerary.create(itinerary_info)
+
+puts "Finished!"
