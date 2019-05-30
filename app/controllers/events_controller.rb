16a56ad65b0f5b3e83238266df61e72c513c01b4
Simone Peer <simone.peer@gmail.com>
2019-05-30 13:09:51 +0100

    Event detail show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,13 +4,14 @@ class EventsController < ApplicationController
     @events = @itinerary.events
     @events = Event.where.not(latitude: nil, longitude: nil)
     @markers = @events.map do |event|
-    {
-      lat: event.latitude,
-      lng: event.longitude
-      # infoWindow: { content: render_to_string(partial: "/events/map_box", locals: { event: event }) }
-      # Uncomment the above line if you want each of your markers to display a info window when clicked
-      # (you will also need to create the partial "/events/map_box")
-    }
+      {
+        lat: event.latitude,
+        lng: event.longitude
+        # infoWindow: { content: render_to_string(partial: "/events/map_box", locals: { event: event }) }
+        # Uncomment the above line if you want each of your markers to display a info window when clicked
+        # (you will also need to create the partial "/events/map_box")
+      }
+    end
   end
 
   def show
