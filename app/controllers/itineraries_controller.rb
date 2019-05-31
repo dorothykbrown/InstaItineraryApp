f49ea9ba3a63f0eb5d8d4c84c8ffb8c764f30de6
Patricia Vila Luz <patriciavilaluz@hotmail.com>
2019-05-31 10:25:16 +0100

    fix events loc

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -15,90 +15,80 @@ class ItinerariesController < ApplicationController
 
     @itin_results = GooglePlacesService.generate_itin(@itinerary.id)
     @markers = @itin_results.map do |event|
-    {
-      lat: event.latitude,
-      lng: event.longitude,
-      infoWindow: render_to_string(partial: "info_window", locals: { property: event })
-    }
+      {
+        lat: event.latitude,
+        lng: event.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { property: event })
+      }
     end
   end
 
   def new
     @itinerary = Itinerary.new
+    authorize @itinerary
+    if params[:query].present?
+      @search = Geocoder.search(params[:query])
+      if @search == []
+        flash[:notice] = "No Search Results for that location"
+        redirect_to root_path
+      else
+        @first_result = @search.first
+        render_markers
+      end
+    else
+      @search = Geocoder.search("lisbon")
+      @first_result = @search.first
+      render_markers
+    end
   end
 
   def create
     @itinerary = Itinerary.new(itinerary_params)
     @itinerary.user = current_user
     authorize @itinerary
-    @itin_results = GooglePlacesService.search(@itinerary.id)
-  end
 
-def new
-  @itinerary = Itinerary.new
-  authorize @itinerary
-  if params[:query].present?
-    @search = Geocoder.search(params[:query])
-    if @search == []
-      flash[:notice] = "No Search Results for that location"
-      redirect_to root_path
+    if @itinerary.save
+      flash[:success] = "Your itinerary parameters have been saved!"
+      @itin_results = GooglePlacesService.search(@itinerary.id)
+      redirect_to user_itinerary_path(current_user, @itinerary)
     else
-      @first_result = @search.first
-      render_markers
+      render :new
     end
-  else
-    @search = Geocoder.search("lisbon")
-    @first_result = @search.first
-    render_markers
   end
-end
 
-def create
-  @itinerary = Itinerary.new(itinerary_params)
-  @itinerary.user = current_user
-  authorize @itinerary
-
-  if @itinerary.save
-    flash[:success] = "Your itinerary parameters have been saved!"
-    redirect_to user_itinerary_path(current_user, @itinerary)
-  else
-    render :new
+  def edit
   end
-end
 
-def edit
-end
-
-def update
-  @itinerary = Itinerary.update(itinerary_params)
-  authorize @itinerary
-  if @itinerary.save
-    redirect_to user_itinerary_path(@itinerary)
-  else
-    render :edit
+  def update
+    @itinerary = Itinerary.update(itinerary_params)
+    authorize @itinerary
+    if @itinerary.save
+      redirect_to user_itinerary_path(@itinerary)
+    else
+      render :edit
+    end
   end
-end
 
-def destroy
-  authorize @itinerary
-  @itinerary.destroy
-end
+  def destroy
+    authorize @itinerary
+    @itinerary.destroy
+  end
 
-private
+  private
 
-def render_markers
-  @markers = [
-    {
-      lat: @first_result.latitude,
-      lng: @first_result.longitude,
-      infoWindow: render_to_string(partial: "new_window", locals: { search: @search.first.display_name })
-    }
-  ]
-end
+  def render_markers
+    @markers = [
+      {
+        lat: @first_result.latitude,
+        lng: @first_result.longitude,
+        infoWindow: render_to_string(partial: "new_window", locals: { search: @search.first.display_name })
+      }
+    ]
+  end
 
-def build_user_cat
-    #get an array from the prevous form
-    #on each create one
+  def build_user_cat
+    # get an array from the prevous form
+    # on each create one
     UserCategory.new
   end
 
